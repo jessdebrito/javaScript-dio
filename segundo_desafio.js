61e3bddd2d997b7c65531a768f68087886b0d148
jessdebrito <debrito.herself@gmail.com>
2024-04-05 21:24:45 -0300

    .

--- segundo_desafio.js
+++ segundo_desafio.js
@@ -1,25 +1,19 @@
 // R$/litro
 const preco__Etanol = 5.79;
 const preco__Gasolina = 6.66;
-
-//  Gasto médio de combustível do carro por KM;
-const kmPorLitro__Ethanol = 10;
-const kmPorLitro__Gasolina = 15;
-
+const kmPorLitro = 10;
 const distanciaKM = 100;
 
-const litrosConsumidos__Etanol = distanciaKM / kmPorLitro__Ethanol;
-const litrosConsumidos__Gasolina = distanciaKM / kmPorLitro__Gasolina;
 
-const gastoViagem__Etanol = (distanciaKM * preco__Etanol);
-const gastoViagem__Gasolina = (distanciaKM * preco__Gasolina);
+const tipoCombustivel = 'Gasolina';
 
 
-/* 
-if (etanol) {
-    console.log(gastoViagem__Etanol);
+const litrosConsumidos = distanciaKM / kmPorLitro;
+
+if (tipoCombustivel === 'Etanol') {
+    const valorGasto = litrosConsumidos * preco__Etanol;
+    console.log(valorGasto.toFixed(2));
 } else {
-    console.log(gastoViagem__Gasolina);
-} */
-
-console.log(gastoViagem__Etanol.toFixed(2));
+    const valorGasto = litrosConsumidos * preco__Gasolina;
+    console.log(valorGasto.toFixed(2));
+}
