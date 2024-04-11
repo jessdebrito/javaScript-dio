9e49ae346a1f8556f3b6c5e9cb717b9f48102049
jessdebrito <debrito.herself@gmail.com>
2024-04-10 23:22:40 -0300

    exercicio1

---
+++ 04 - Objetos e Classes/exercicio1.js
@@ -0,0 +1,7 @@
+/*  node ./exercicio1.js
+
+
+1 - Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+Crie um método que dado a quantidade de quilometros e o preço do combustível nos dê o valor gasto em reais para realizar este percurso.
+ */
