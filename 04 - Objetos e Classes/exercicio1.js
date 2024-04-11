86002c923a7f1720c7116e4722e23d0bd3346e4d
jessdebrito <debrito.herself@gmail.com>
2024-04-10 23:28:32 -0300

    exercicio1

--- 04 - Objetos e Classes/exercicio1.js
+++ 04 - Objetos e Classes/exercicio1.js
@@ -5,3 +5,15 @@
 Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
 Crie um método que dado a quantidade de quilometros e o preço do combustível nos dê o valor gasto em reais para realizar este percurso.
  */
+
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca, cor, gastoMedioPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+}
