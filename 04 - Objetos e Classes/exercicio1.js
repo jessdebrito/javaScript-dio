4e51cf38d2d01278a9c2366e8f1e14b1f736f522
jessdebrito <debrito.herself@gmail.com>
2024-04-10 23:37:46 -0300

    Exercício 1 : calcularGastoDePercurso

--- 04 - Objetos e Classes/exercicio1.js
+++ 04 - Objetos e Classes/exercicio1.js
@@ -17,11 +17,13 @@ class Carro {
         this.gastoMedioPorKm = gastoMedioPorKm;
     }
 
+    calcularGastoDePercurso(distanciaEmKm, precoCombustivel){
+        return distanciaEmKm * this.gastoMedioPorKm * precoCombustivel;
+    }
 }
 
 const uno = new Carro('Fiat','Prata', 1/12);
-const uno2 = new Carro('Fiat','Azul', 1/12);
+;
+console.log(uno.calcularGastoDePercurso(70, 5));
 
-console.log(uno);
 
-console.log(uno2);
